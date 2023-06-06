58eb7bcee3d441d87e1050c7a09c7e14b9fe5907
javieravidal <126521755+javieravidal@users.noreply.github.com>
2023-06-06 11:41:33 -0400

    Updated app structure

--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
-
+import Container from './Container.js';
+import Search from './Search.js'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Container />
   </React.StrictMode>
 );
 
